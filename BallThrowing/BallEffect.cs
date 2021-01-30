a5e99d66469abad37c30e75a56b44a2c5b572c65
Seoha Kim <46925617+seoha-kim@users.noreply.github.com>
2021-01-30 20:13:05 +0900

    2021-1-30 updates

---
+++ BallThrowing/BallEffect.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallEffect : MonoBehaviour
+{
+    public ParticleSystem particle;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag.Equals("Player")) return;
+        Instantiate(particle, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
